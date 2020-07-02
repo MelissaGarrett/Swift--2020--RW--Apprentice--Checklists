c09ffa3e5c4929b419d526dd6ad4dd2a4f7a3de9
MelissaGarrett <melissa_garrett@hotmail.com>
2020-07-01 22:49:46 -0500

    Added a Text Field to the AddItem VC; Disabled the Done buttons if there is no text in the Text Field.

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textField.becomeFirstResponder()
     }
     
     // MARK:- Actions
@@ -21,7 +29,42 @@ class AddItemViewController: UITableViewController {
         navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func done() {
+    @IBAction func done() {        
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK:- Table View Delegates
+
+    // To disable the user from selecting a row
+    // Also, in Storyboard: Table View Cell->Atributes->Selection = None
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    
+    // To disable the Done button if Text Field is empty
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        
+        return true
+    }
+    
+    // To disable the Done button if user tapped the Clear Button ('x')
+    // to clear out the Text Field
+    // DOESN'T WORK EVERY TIME; CHECK OUT textFieldEditChanged()
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        
+        return true
+    }
+    
 }
